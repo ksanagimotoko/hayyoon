e14435725c1a3ac49cb23a470750bfd020326c3f
pjhnoble <pjhnoble@dadora.kr>
2024-02-20 11:23:41 +0900

    코드정리

---
+++ util.js
@@ -0,0 +1,8 @@
+
+    function goToSeoul () {
+        map.flyTo({
+            center: [127.0016985, 37.413294],
+            essential: true // this animation is considered essential with respect to prefers-reduced-motion
+        });
+
+    }
